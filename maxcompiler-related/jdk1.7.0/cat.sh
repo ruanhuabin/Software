515925292bf864fbb2d143d175d6e41fb8a5d847
root <root@fpgaworkstation.(none)>
2015-06-28 23:54:20 +0800

    ...

---
+++ maxcompiler-related/jdk1.7.0/cat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat jdk-7u79-linux-x64.gzaa >> jdk-7u79-linux-x64.gz
+cat jdk-7u79-linux-x64.gzab >> jdk-7u79-linux-x64.gz
+cat jdk-7u79-linux-x64.gzac >> jdk-7u79-linux-x64.gz
+cat jdk-7u79-linux-x64.gzad >> jdk-7u79-linux-x64.gz
+cat jdk-7u79-linux-x64.gzae >> jdk-7u79-linux-x64.gz
+cat jdk-7u79-linux-x64.gzaf >> jdk-7u79-linux-x64.gz
+cat jdk-7u79-linux-x64.gzag >> jdk-7u79-linux-x64.gz
+cat jdk-7u79-linux-x64.gzah >> jdk-7u79-linux-x64.gz
---
+++ maxcompiler-related/maxeleros2014.2/cat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cat maxeleros-2014.2-1.el6.x86_64.rpmaa >> maxeleros-2014.2-1.el6.x86_64.rpm
+cat maxeleros-2014.2-1.el6.x86_64.rpmab >> maxeleros-2014.2-1.el6.x86_64.rpm
+cat maxeleros-2014.2-1.el6.x86_64.rpmac >> maxeleros-2014.2-1.el6.x86_64.rpm
+cat maxeleros-2014.2-1.el6.x86_64.rpmad >> maxeleros-2014.2-1.el6.x86_64.rpm
+cat maxeleros-2014.2-1.el6.x86_64.rpmae >> maxeleros-2014.2-1.el6.x86_64.rpm
+cat maxeleros-2014.2-1.el6.x86_64.rpmaf >> maxeleros-2014.2-1.el6.x86_64.rpm
