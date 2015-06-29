94c8c06d883cfab4dc92b41dc767a685c0a38059
root <root@fpgaworkstation.(none)>
2015-06-29 17:53:11 +0800

    ...

---
+++ maxcompiler-related/maxcompiler2014.2/cat.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cat maxcompiler-2014.2-installer.tar.gzaa >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzab >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzac >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzad >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzae >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzaf >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzag >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzah >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzai >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzaj >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzak >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzal >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzam >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzan >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzao>> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzap >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzaq >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzar >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzas >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzat >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzau >> maxcompiler-2014.2-installer.tar.gz
+cat maxcompiler-2014.2-installer.tar.gzav >> maxcompiler-2014.2-installer.tar.gz
